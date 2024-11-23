e779e9c467b9e0e50007f26b8dc6a31c3cf31047
sdfd32 <rokmcjwh@gmail.com>
2024-11-24 00:01:04 +0900

    Add files via upload

--- script/c10054422.lua
+++ script/c10054422.lua
@@ -52,7 +52,7 @@ function s.dotteop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(1)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
     end
     if dotte<=3 then
@@ -62,7 +62,7 @@ function s.dotteop(e,tp,eg,ep,ev,re,r,rp)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
         e1:SetValue(1)
         e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
         tc:RegisterEffect(e1)
     end
     else end
